c68ebb934127df6be01dffcdf9cf3537bbba1dd1
Nicolas Aponte <nico.aponte98@hotmail.com>
2020-10-13 17:06:14 -0500

    Express Example

---
+++ index.js
@@ -0,0 +1,21 @@
+//En este ejercicio se inicia un servidor con ayuda de Express
+const express = require('express');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();//Decimos que la app va a usar Express Node Module
+
+app.use((req,res,next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>')
+}); //Next es usado como middleware adicional
+
+const server = http.createServer(app);
+
+server.listen(port,hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`)
+});
