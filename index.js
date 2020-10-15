c70df9d9aab7704f11126dd0d543624f56ff3bfb
Nicolas Aponte <nico.aponte98@hotmail.com>
2020-10-14 20:14:38 -0500

    Express Simple REST

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-//En este ejercicio se inicia un servidor con ayuda de Express
+
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const hostname = 'localhost';
+const hostname = '192.168.1.7';
 const port = 3000;
 
-const app = express();//Decimos que la app va a usar Express Node Module
+const app = express();
+
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next) => {//Este codigo se ejectua por defecto siempre que se requiera /dishes, es decir, se ejecuta siempre antes de los otros metodos REST
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();//Continua con la siguiente funciony pasa req y res
+});
+
+app.get('/dishes',(req,res,next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes',(req,res,next) => {
+    res.end('Will  add the dish: '+ req.body.name + ' with details: '+ req.body.description);
+});
+
+app.put('/dishes',(req,res,next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes',(req,res,next) => {
+    res.end('Deleting all the dishes to you!');
+});
+//-----------------------------------------------------------------
+app.get('/dishes/:dishId',(req,res,next) => {
+    res.end('Will send details of the dish: '+req.params.dishId+' to you!');
+});
+
+app.post('/dishes/:dishId',(req,res,next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+req.params.dishId);
+});
+
+app.put('/dishes/:dishId',(req,res,next) => {
+    res.write('Updatiing the dish: '+ req.params.dishId + ' \n')
+    res.end('Will update the dish: '+req.body.name + ' with details: '+req.body.description);
+});
+
+app.delete('/dishes/:dishId',(req,res,next) => {
+    res.end('Deleting dish: '+req.params.dishId);
+});
 
 app.use(express.static(__dirname+'/public'))
 
@@ -15,7 +59,7 @@ app.use((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>')
-}); //Next es usado como middleware adicional
+});
 
 const server = http.createServer(app);
 
